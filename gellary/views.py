8d7c6e6f5cf8f92cc152fb896224e9f83e056743
Sabbirdiu <sabbir.s.dk7@gmail.com>
2021-05-06 21:24:22 +0600

    added all files

--- gellary/views.py
+++ gellary/views.py
@@ -7,8 +7,8 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect
 from .forms import ImageForm
 from .models import ImageAlbum
-from django.views.generic import CreateView,ListView,DetailView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView,ListView,DetailView,UpdateView,DetailView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from django.contrib.auth.models import User
 
 class PostCreateView(LoginRequiredMixin,CreateView):
@@ -51,6 +51,31 @@ class UserAlbumListView(ListView):
 #     def get_queryset(self):
 #         user = get_object_or_404(User, username=self.kwargs.get('username'))
 #         return ImageAlbum.objects.filter(author=user).order_by('-date_posted')
+class  AlbumUpdateView(LoginRequiredMixin, UserPassesTestMixin,UpdateView):
+    model = ImageAlbum
+    fields=['title','cover_image','image','image2','image3','image4','image5','image6','image7','image8','image9','image10','is_public']    
+    template_name='gellary/update_album.html'
+   
+   
+    def form_valid(self,form):#without author,other user can not update the post
+        form.instance.author = self.request.user
+        return super().form_valid(form)        
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user== post.author:
+            return True
+        return False 
+class AlbumDeleteView(LoginRequiredMixin, UserPassesTestMixin,DeleteView):
+    model = ImageAlbum
+    template_name = 'gellary/delete_post.html'
+    success_url='/'       
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user== post.author:
+            return True
+        return False
 def gellary(request):
     current_user = request.user
 

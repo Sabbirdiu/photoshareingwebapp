e360137d15f78d5eebfd184619172f44c3c4c1e1
Sabbirdiu <sabbir.s.dk7@gmail.com>
2021-05-06 21:50:19 +0600

    finall update

--- gellary/urls.py
+++ gellary/urls.py
@@ -1,12 +1,12 @@
 
 from django.urls import path
-from .views import PostCreateView,home,PostDetailView,UserAlbumListView,gellary,AlbumUpdateView,AlbumDeleteView
+from .views import AlbumCreateView,home,AlbumDetailView,UserAlbumListView,gellary,AlbumUpdateView,AlbumDeleteView
 urlpatterns = [
    path('',home),
-   path('<int:pk>/',PostDetailView.as_view(),name ='post-detail'),
-   path('post_create/',PostCreateView.as_view(),name="create"),
+   path('<int:pk>/',AlbumDetailView.as_view(),name ='post-detail'),
+   path('post_create/',AlbumCreateView.as_view(),name="create"),
    path('user/<str:username>', UserAlbumListView.as_view(), name='user-posts'),
    path('gellary/', gellary, name='gellary'),
    path('album/<int:pk>/update_post/',AlbumUpdateView.as_view(),name ='album-update'),
-     path('post/<int:pk>/delete_post/',AlbumDeleteView.as_view(),name ='post-delete'),
+   path('post/<int:pk>/delete_post/',AlbumDeleteView.as_view(),name ='post-delete'),
 ]
--- gellary/views.py
+++ gellary/views.py
@@ -11,7 +11,7 @@ from django.views.generic import CreateView,ListView,DetailView,UpdateView,Detai
 from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from django.contrib.auth.models import User
 
-class PostCreateView(LoginRequiredMixin,CreateView):
+class AlbumCreateView(LoginRequiredMixin,CreateView):
     model = ImageAlbum
     fields=['title','cover_image','image','image2','image3','image4','image5','image6','image7','image8','image9','image10','is_public']    
     template_name='gellary/new_album.html'
@@ -19,20 +19,15 @@ class PostCreateView(LoginRequiredMixin,CreateView):
     def form_valid(self,form):#without a author other user cannot create a post
         form.instance.author = self.request.user
         return super().form_valid(form)
-# class PostListView(ListView):
-#     model = ImageAlbum
-#     template_name = 'gellary/home.html'
-#     context_object_name = 'posts'
-#     ordering = ['-date_posted']
-#     paginate_by = 2
+
 def home(request):
     context = {
         'posts':ImageAlbum.objects.filter(is_public=True).order_by('-date_posted')
     }
     return render(request,'gellary/home.html',context)
-class PostDetailView(DetailView):
+class AlbumDetailView(DetailView):
     model = ImageAlbum  
-    template_name = 'gellary/detail_view.html'
+    template_name = 'gellary/detail_album.html'
 class UserAlbumListView(ListView):
     model = ImageAlbum
     template_name = 'gellary/user_album.html'  
@@ -42,15 +37,7 @@ class UserAlbumListView(ListView):
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
         return ImageAlbum.objects.filter(author=user,is_public=True).order_by('-date_posted')
-# class Gellary(ListView):
-#     model = ImageAlbum
-#     template_name = 'gellary/gellary.html'  
-#     context_object_name = 'gellary'
-#     paginate_by = 5
 
-#     def get_queryset(self):
-#         user = get_object_or_404(User, username=self.kwargs.get('username'))
-#         return ImageAlbum.objects.filter(author=user).order_by('-date_posted')
 class  AlbumUpdateView(LoginRequiredMixin, UserPassesTestMixin,UpdateView):
     model = ImageAlbum
     fields=['title','cover_image','image','image2','image3','image4','image5','image6','image7','image8','image9','image10','is_public']    
@@ -68,7 +55,7 @@ class  AlbumUpdateView(LoginRequiredMixin, UserPassesTestMixin,UpdateView):
         return False 
 class AlbumDeleteView(LoginRequiredMixin, UserPassesTestMixin,DeleteView):
     model = ImageAlbum
-    template_name = 'gellary/delete_post.html'
+    template_name = 'gellary/delete_album.html'
     success_url='/'       
 
     def test_func(self):
@@ -83,4 +70,4 @@ def gellary(request):
     context = {
         'gellary':gellary
     }
-    return render(request,'gellary/gellary.html',context)
+    return render(request,'gellary/my_gellary.html',context)
